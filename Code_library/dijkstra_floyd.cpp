566f0ea84ae19fcead2facc6d1628408a31af010
Fady-Latif <fady.sherif@aucegypt.edu>
2026-05-15 23:31:41 +0300

    code addition

--- Code_library/dijkstra_floyd.cpp
+++ Code_library/dijkstra_floyd.cpp
@@ -2,9 +2,9 @@
 * WARNING: Please do not remove or modify this comment block.
  *
  * Student Information:
- * Name: ______________________________________
- * Student ID: __________________________________
- * Section Number: ______________________________
+ * Name: Fady Latif
+ * Student ID: 900241677
+ * Section Number: 2
  *
  * Instructions:
  * - Fill out your name, student ID, and section number above.
@@ -24,9 +24,26 @@ using namespace std;
 vector<int> dijkstra(int src) {
     vector<int> dist(n, 1e9);
 
-    // TODO: Initialize priority queue
-    // TODO: Set dist[src] = 0 and push into pq
-    // TODO: Write Dijkstra main loop
+  
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+    dist[src] = 0;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+
+      
+        if (d > dist[u]) continue;
+
+        for (auto [v, w] : adjList[u]) {
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
 
     return dist;
 }
@@ -34,7 +51,8 @@ vector<int> dijkstra(int src) {
 vector<vector<int>> repeatedDijkstra() {
     vector<vector<int>> all(n, vector<int>(n));
 
-    // TODO: Run dijkstra(i) for all i
+    for (int i = 0; i < n; i++)
+        all[i] = dijkstra(i);
 
     return all;
 }
@@ -42,7 +60,12 @@ vector<vector<int>> repeatedDijkstra() {
 vector<vector<int>> floydWarshall() {
     vector<vector<int>> dist = adjMatrix;
 
-    // TODO: Implement triple loop
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (dist[i][k] < 1e9 && dist[k][j] < 1e9)
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
 
     return dist;
 }
