b1c7d628d089ee1e0de6f621c697dd6432808e93
Fady-Latif <fady.sherif@aucegypt.edu>
2026-05-16 00:12:30 +0300

    final submission

---
+++ build/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Lenovo/Desktop/asignment/Applied-Data-Structure-Assignment-3-Dijkstra-shortest-path/Google_tests/lib
+# Build directory: C:/Users/Lenovo/Desktop/asignment/Applied-Data-Structure-Assignment-3-Dijkstra-shortest-path/build/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
